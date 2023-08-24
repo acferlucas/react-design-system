9b984fab33c60b44decd0ec9540b16aa4fcd8007
acferlucas <acferlucas.dev@gmail.com>
2023-08-24 00:17:29 -0300

    create input component

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,38 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, TextInput, TextInputProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  decorators: [
+    (story) => {
+      return (
+        <Box
+          as={'label'}
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text>Email Address</Text>
+          {story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+import { TextContainer, Prefix, Input } from './styles'
+
+export type TextInputProps = ComponentProps<typeof Input> & {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...rest }: TextInputProps) {
+  return (
+    <TextContainer>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+      <Input {...rest} />
+    </TextContainer>
+  )
+}
