28c21f8efc7c3c85d87880739727438ae22cf99e
acferlucas <acferlucas.dev@gmail.com>
2024-03-01 17:51:01 -0300

    fix and updates

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @acfer-ignite-ui/react
 
+## 3.0.0
+
+### Major Changes
+
+- update
+
 ## 2.0.0
 
 ### Major Changes
--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,17 +1,19 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, forwardRef, ElementRef } from 'react'
 import { TextContainer, Prefix, Input } from './styles'
 
 export type TextInputProps = ComponentProps<typeof Input> & {
   prefix?: string
 }
 
-export function TextInput({ prefix, ...rest }: TextInputProps) {
-  return (
-    <TextContainer>
-      {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input {...rest} />
-    </TextContainer>
-  )
-}
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, ...rest }: TextInputProps, ref) => {
+    return (
+      <TextContainer>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input ref={ref} {...rest} />
+      </TextContainer>
+    )
+  },
+)
 
 TextInput.displayName = 'TextInput'
